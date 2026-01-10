c18819cb918b5476bf013e08ba86c2fb4879ea6e
Mintograde <mintograde@gmail.com>
2026-01-10 08:54:47 -0500

    Avoid 2MB/10s memory leak by reusing httpx client within api_client

--- api_client.py
+++ api_client.py
@@ -1,35 +1,37 @@
 import os
 from pprint import pprint
-
+import time
 import httpx
 
 base_url = os.getenv("API_BASE_URL")
 api_key = os.getenv("API_KEY")
 
-def send_game_status(status):
+
+def send_game_status(client, status):
 
     if 'game_id' not in status:
         return
+
+    try:
+        response = client.post('/game-status', json=status)
+        response.raise_for_status()
+    except Exception as e:
+        pprint(status)
+        raise e
+
+
+def watch_queue(game_status_queue):
 
     headers = {
         "Content-Type": "application/json",
         'x-api-key': api_key,
     }
 
-    try:
-        response = httpx.post(f'{base_url}/game-status', json=status, headers=headers)
-        response.raise_for_status()
-    except Exception as e:
-        pprint(status)
-        raise
-
-
-def watch_queue(game_status_queue):
-
-    while True:
-        status = game_status_queue.get()
-        send_game_status(status)
-        print(f"Sent status: {status}")
+    with httpx.Client(base_url=base_url, headers=headers, timeout=10.0) as client:
+        while True:
+            status = game_status_queue.get()
+            send_game_status(client, status)
+            print(f"Sent status: {status}")
 
 
 def start_client(game_status_queue):
@@ -39,3 +41,4 @@ def start_client(game_status_queue):
             watch_queue(game_status_queue)
         except Exception as e:
             print(f"API Client Error: {e}")
+            time.sleep(5)
