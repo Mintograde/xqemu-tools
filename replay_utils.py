7da3b6732aaf3b26818da98e97c177d08e6555d6
Mintograde <mintograde@gmail.com>
2026-09-11 08:03:56 -0400

    Preserve broken surfaces in python-generated replays

--- replay_utils.py
+++ replay_utils.py
@@ -49,6 +49,7 @@ necessary_fields = dict(
             scenario_name = {},
             checksum = {},
         ),
+        broken_surfaces={},
         damage_counts={},
         current_time={},
         start_time={},
@@ -116,7 +117,6 @@ necessary_fields = dict(
 
 necessary_fields_live = necessary_fields['ticks'].copy()
 necessary_fields_live.update(dict(
-    broken_surfaces={},
     events=necessary_fields['events'].copy(),
     spawns=necessary_fields['spawns'].copy(),
     items=necessary_fields['items'].copy(),
